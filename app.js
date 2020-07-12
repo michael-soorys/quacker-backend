5fa10091f5f6f3d01f2982827b9a616962c85831
michael-soorys <michael@soorys.com>
2020-07-12 11:06:42 +0300

    Revert "ES6-ify the boilerplate app"
    This reverts commit 105db3e10e056b2e8d77f9572d66f4bdfee49aa4.

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-const app = express();
+var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,5 +15,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-export default app;
+module.exports = app;
