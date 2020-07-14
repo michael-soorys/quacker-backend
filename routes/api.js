76b039188570e8ad84377860c6940619a8a07c86
michael-soorys <michael@soorys.com>
2020-07-14 10:21:26 +0300

    Create route structure for the API

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var router = express.Router();
+var api = express.Router();
 var passportMiddleware = require('../middleware/passportMiddleware');
 
 const mongoose = require('mongoose');
@@ -7,13 +7,13 @@ const mongoose = require('mongoose');
 // Destructure passport middleware
 const { jwtChecker, passportRegister, passportLogin } = passportMiddleware;
 
-router.get('/', function (req, res, next) {
+api.get('/explore', function (req, res, next) {
 	res.send('test');
 });
 
 // Login and registration routes
-router.post('/register', passportRegister);
-router.post('/login', passportLogin);
+api.post('/register', passportRegister);
+api.post('/login', passportLogin);
 
 /**
  *
@@ -21,10 +21,52 @@ router.post('/login', passportLogin);
  *
  */
 
-router.use(jwtChecker); // Verify JWT token
+api.use(jwtChecker); // Verify JWT token
 
-router.get('/test', (req, res, next) => {
+api.get('/test', (req, res, next) => {
 	res.send('found');
 });
 
-module.exports = router;
+// Explore page for logged in user
+api.get('/user/explore', (req, res, next) => {});
+
+// Access to the user's followers
+api.get('/user/followers', (req, res, next) => {});
+
+// Access to the user's following
+api.get('/user/following', (req, res, next) => {});
+
+// Get other random users that the user doesn't follow
+api.get('/users/random', (req, res, next) => {});
+
+// Get other random quacks that the user doesn't follow
+api.get('/quacks/random', (req, res, next) => {});
+
+// Get the user's feed
+api.get('/feed', (req, res, next) => {});
+
+// Get the data of the user with an ID of :id. This includes the user's own id as a possibility
+api.get('/user/:id', (req, res, next) => {});
+
+// Get the user's own profile
+api.get('/profile', (req, res, next) => {});
+
+// Post a quack
+api.post('/quack', (req, res, next) => {});
+
+// Delete a quack
+api.delete('/quack', (req, res, next) => {});
+
+// Follow a user
+api.post('/follow', (req, res, next) => {});
+
+// Unfollow a user
+api.delete('/follow', (req, res, next) => {});
+
+// Post a quack, includes requacks
+api.post('/quack/comment', (req, res, next) => {});
+
+// Like a quack
+api.post('/quack/like', (req, res, next) => {});
+
+module.exports = api;
